50f27c5dcb1453f9756554f1d04183c9e1d24f7f
강수나 <acrnm148@gmail.com>
2023-09-18 23:17:11 +0900

    feat: AsyncConfig 내에서 RejectedException 처리 옵션 설정

--- Async/src/main/java/com/async/myapp/async/web/config/AsyncConfig.java
+++ Async/src/main/java/com/async/myapp/async/web/config/AsyncConfig.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync    //비동기 활성화
 @Configuration  //클래스를 Bean으로 등록
@@ -14,10 +15,11 @@ public class AsyncConfig extends AsyncConfigurerSupport {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor(); //executor : 실행자
-        executor.setCorePoolSize(5);    //기본 실행대기하는 thread 수
-        executor.setMaxPoolSize(10);    //동시 동작하는 최대 thread 수
-        executor.setQueueCapacity(30);  //thread 생성 요청 시 corePoolSize 또는 MaxPoolSize가 초과되었을 때, 해당 요청을 Queue에 저장. Queue가 수용할 수 있는 최대 Thread 수
+        executor.setCorePoolSize(1);    //기본 실행대기하는 thread 수
+        executor.setMaxPoolSize(1);    //동시 동작하는 최대 thread 수
+        executor.setQueueCapacity(0);  //thread 생성 요청 시 corePoolSize 또는 MaxPoolSize가 초과되었을 때, 해당 요청을 Queue에 저장. Queue가 수용할 수 있는 최대 Thread 수
         executor.setThreadNamePrefix("EVENT-");    //thread 생성 시 접두사
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
     }
